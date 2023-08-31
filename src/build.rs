eb2fd55dbaafc92672c235676d24664f5fb11288
tobiaslieber <14908876+tobiaslieber@users.noreply.github.com>
2023-08-31 15:38:27 +0200

    add linkage information from pkg-config

---
+++ src/build.rs
@@ -0,0 +1,9 @@
+use pkg_config::Config;
+
+fn main() {
+    if std::env::var_os("CARGO_FEATURE_X11").is_some() {
+        Config::new().probe("xkbcommon-x11").unwrap();
+    } else {
+        Config::new().probe("xkbcommon").unwrap();
+    }
+}
